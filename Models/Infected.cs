220e0bf2dd71eaa32991354dd31785f8f12652f4
elador <elador.oe@cs,colman.ac.il>
2020-05-24 16:21:36 +0300

    users

--- Models/Infected.cs
+++ Models/Infected.cs
@@ -5,7 +5,29 @@ using System.Web;
 
 namespace CovidApp.Models
 {
-    public class Infected
+    public class User
     {
+
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public MyGender Gender { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+        
+        public bool Diagnosed { get; set; }
+
+
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CovidApp.Models
+{
+    public class User
+    {
+
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public MyGender Gender { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool Diagnosed { get; set; }
+
+
+    }
+
+    public class MyGender
+    {
+    }
+}
